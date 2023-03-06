0e01ae952ec89f40b5443b3f5ace9dc9e10f1de5
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-03-06 20:07:34 +0100

    added default city to the weather props

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
@@ -77,8 +77,8 @@ export default function Weather() {
     );
   } else {
     const apiKey = "5fb4oa610201e8b3c770fffbaee96fft";
-    let city = "Oslo";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
