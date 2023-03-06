5db611406a4d8efa5ae2b8b43c5e40361bfc5268
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-03-06 20:19:44 +0100

    added background

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Oslo" />
+        <Weather defaultCity="New York" />
         <Footer />
       </div>
     </div>
