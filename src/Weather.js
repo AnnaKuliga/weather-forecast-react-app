a902631a98ba40f15b5dc5a2303d93774727205f
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-03-06 18:56:10 +0100

    styled the weather body

--- src/Weather.js
+++ src/Weather.js
@@ -27,17 +27,22 @@ export default function Weather() {
       <h2>Norway</h2>
       <ul>
         <li>Wednesday, 07:00</li>
-        <li>Cloudy</li>
+        <li>Sunny</li>
       </ul>
-      <div className="row">
-        <div className="col-6">
+      <div className="grid grid-3-columns">
+        <div className="currentTemperature">
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="mainIcon">
           <img
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAUhJREFUeNrt230NgzAQh2GkIAUJyJgMJCABCZNQKcxBHdyOpFkyEkYGV9oL75Lff2Rwz8pXe2tEpLlzGgAAAAAAAAAAYHsDg4+8Hp1m0ATNrJGNzGmbZdvOZN+lAFLRkyb+KHgvMX1H5wYgFR5OFL2VcATiMgA9uFbzzFD4Oss+2qoA9ID6k0P9yKnRVwGgBzJeWPg6Y1GAdIGSwpmKAFRS/C5CFoDCw/6v08EcIF3wpNL0WQHSrS5WDBDXt0hrgGfFxX+eE7IApCc8cZIuB0BwBBBMAZz9+l+jwApgcggwWQJEhwDRBMDp8P+cBhYAg2OAwQIgOAYIFgCzY4DZAkBcB4C7AywvcIwAAADgNsiDEI/CvAzxOsyECFNiTIoyLc7CCEtjLI6yPE6DBC0yNEnRJkejJK2yNEvTLs8fJgAAAAAAAADg1nkDlR7XfJiH1ggAAAAASUVORK5CYII="
             alt="Sunny"
+            className="float-left"
           />
-          6°
         </div>
-        <div className="col-6">
+
+        <div className="extraDescription">
           <ul>
             <li>Precipitation: 15%</li>
             <li>Humidity: 4%</li>
