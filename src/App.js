cd477e3f7f6f7aba001afcdd2773c0e08913f4ca
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-03-07 12:14:05 +0100

    changed the default city

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Oslo" />
         <Footer />
       </div>
     </div>
